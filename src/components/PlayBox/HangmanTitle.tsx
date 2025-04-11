9b07e9ad23b1ee9885ee50f3867c116635b27031
DIvyanshuddev <divyanshu311098@gmail.com>
2025-04-11 15:51:41 -0700

    change the font size

--- src/components/PlayBox/HangmanTitle.tsx
+++ src/components/PlayBox/HangmanTitle.tsx
@@ -5,11 +5,16 @@ const styles = {
         fontSize:{
             lg:40,
             md:40,
-            sm:30,
-            xs:30
+            sm:35,
+            xs:35
         },
         color: "white",
-        "-webkit-text-stroke": "2px black",
+        "-webkit-text-stroke": {
+            lg:"2px black",
+            md:"2px black",
+            sm:"1px black",
+            xs:"1px black"
+        },
         fontWeight: "bolder",
         fontFamily: "Berlin Sans FB",
         position: "absolute",
@@ -22,11 +27,16 @@ const styles = {
         fontSize: {
             lg:40,
             md:40,
-            sm:30,
-            xs:30
+            sm:35,
+            xs:35
         },
         color: "white",
-        "-webkit-text-stroke": "2px black",
+        "-webkit-text-stroke": {
+            lg:"2px black",
+            md:"2px black",
+            sm:"1px black",
+            xs:"1px black"
+        },
         fontWeight: "bolder",
         fontFamily: "Berlin Sans FB",
         position: "absolute",
@@ -41,10 +51,15 @@ const styles = {
         fontSize: {
             lg:80,
             md:80,
-            sm:60,
-            xs:60
+            sm:70,
+            xs:70
         },
-        "-webkit-text-stroke": "3px black",
+        "-webkit-text-stroke": {
+            lg:"3px black",
+            md:"3px black",
+            sm:"2px black",
+            xs:"2px black"
+        },
         color: "white",
         fontWeight: "bolder",
         background: "-webkit-linear-gradient(#cc33ff,#ffffcc)",
